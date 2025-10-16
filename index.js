0192792e2e4df3034fb376aa51e6c308e3d212eb
Koushik Kar <koushikkar712@gmail.com>
2025-10-17 00:44:19 +0530

    Added Cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { UserAuthenticationHandler, userPreDataHandler } = require('./middlewares
 ConnectDb();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://todofrontend-t9qq.onrender.com",
     credentials: true,
 }));
 
